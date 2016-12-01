cd26701e7fe1395857846105b22ea3a343fd8726
Bryan Hunt (binarytemple) <bryan.hunt@bet365.com>
2016-12-01 12:05:48 +0000

    Update 01-bootstrap-schemas.sh

--- poststart.d/01-bootstrap-schemas.sh
+++ poststart.d/01-bootstrap-schemas.sh
@@ -3,8 +3,8 @@
 if [ "$RIAK_FLAVOR" == "TS" ]; then
   # Create TS buckets
   echo "Looking for CREATE TABLE schemas in $SCHEMAS_DIR..."
-  for f in $(find $SCHEMAS_DIR -name *.sql -print); do
-    BUCKET_NAME=$(basename -s .sql $f)
+  for f in $(find $SCHEMAS_DIR -name *.ddl -print); do
+    BUCKET_NAME=$(basename -s .ddl $f)
     BUCKET_DEF=$(cat $f)
     $RIAK_ADMIN bucket-type create $BUCKET_NAME '{"props":{"table_def":"'$BUCKET_DEF'"}}'
     $RIAK_ADMIN bucket-type activate $BUCKET_NAME
